0f63303ba22bf224dbfd8f3292aeff58ac54f3fb
Ronin <bhabyaprakash01@gmail.com>
2024-10-09 14:32:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ load_dotenv()
 API_ID = 20948356
 API_HASH = "6b202043d2b3c4db3f4ebefb06f2df12"
 
-BOT_TOKEN=7271861239:AAHbKVvshC9Jp8vAx9moL70AY751mG4M1C8
+BOT_TOKEN= 7271861239:AAHbKVvshC9Jp8vAx9moL70AY751mG4M1C8
 MONGO_DB_URI = "mongodb+srv://ComboBot:ComboBot@combobot.f9czz.mongodb.net/?retryWrites=true&w=majority&appName=ComboBot"
 
 OWNER_ID = 7337748194
