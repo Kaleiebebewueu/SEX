fe459f9053e16745d840dbeba9b233a3b3d2fca0
Ronin <bhabyaprakash01@gmail.com>
2024-10-09 14:54:27 +0530

    Update stats.py

--- StringGen/modules/stats.py
+++ StringGen/modules/stats.py
@@ -2,11 +2,11 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from config import OWNER_ID
-from StringGen import Anony
-from StringGen.utils import get_served_users
+from Esclipse import app
+from Esclipse.utils import get_served_users
 
 
-@Anony.on_message(filters.command(["stats", "users"]) & filters.user(OWNER_ID))
+@app.on_message(filters.command(["stats", "users"]) & filters.user(OWNER_ID))
 async def get_stats(_, message: Message):
     users = len(await get_served_users())
     await message.reply_text(f"» ᴄᴜʀʀᴇɴᴛ sᴛᴀᴛs ᴏғ {Anony.name} :\n\n {users} ᴜsᴇʀs")
