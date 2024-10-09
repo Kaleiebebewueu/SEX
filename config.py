f3247316e4239394df2986f56d2f194f22044e5c
Ronin <bhabyaprakash01@gmail.com>
2024-10-09 14:08:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+API_ID = 20948356
+API_HASH = "6b202043d2b3c4db3f4ebefb06f2df12"
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+BOT_TOKEN=7271861239:AAHbKVvshC9Jp8vAx9moL70AY751mG4M1C8
+MONGO_DB_URI = "mongodb+srv://ComboBot:ComboBot@combobot.f9czz.mongodb.net/?retryWrites=true&w=majority&appName=ComboBot"
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
-
-OWNER_ID = int(getenv("OWNER_ID", 1356469075))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/phoenixXsupport")
+OWNER_ID = 7337748194
+SUPPORT_CHAT = "https://t.me/Mystic_legion"
+SUPPORT_CHANNEL = "https://t.me/Mystic_movies" 
